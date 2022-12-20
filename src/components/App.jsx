12fd2e6a3571ee4e9eef4d0c3f10ffddd8af0a0d
Adilnurlan <nurlan.adil98@gmail.com>
2022-12-20 23:38:55 +0600

    second

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,96 +1,70 @@
-import React, { Component } from 'react';
-import ContactForm from './ContactForm/ContactForm';
+import React, { useState, useEffect } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
-import { nanoid } from 'nanoid';
 import s from './App.module.css';
 
-export default class App extends Component {
-  state = {
-    contacts: [
+export const App = () => {
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) || [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+    ]
+  );
+  const [filter, setFilter] = useState('');
 
-  formSubmit = ({ name, number }) => {
-    const contact = {
-      id: nanoid(),
-      name,
-      number,
-    };
-    const isExist = this.state.contacts.find(
+  const formSubmit = contact => {
+    const isExist = contacts.find(
       cont =>
         cont.name.toLowerCase().trim() === contact.name.toLowerCase().trim()
     );
     if (isExist) {
-      return alert(`${name} is already in contacts`);
-    } else if ((name.trim() === '', number.trim() === '')) {
+      return alert(`${contact.name} is already in contacts`);
+    } else if ((contact.name.trim() === '', contact.number.trim() === '')) {
       return alert('Заполните все поля');
     }
-    this.setState(({ contacts }) => ({
-      contacts: [contact, ...contacts],
-    }));
+    setContacts(prevContacts => [...prevContacts, contact]);
   };
 
-  handleInputChange = evt => {
-    this.setState({
-      filter: evt.currentTarget.value,
-    });
-    this.findContact();
+  const handleInputChange = ({ currentTarget: { value } }) => {
+    setFilter(value);
+    // this.setState({
+    //   filter: evt.currentTarget.value,
+    // });
+    findContact();
   };
 
-  findContact = () => {
-    const { filter, contacts } = this.state;
+  const findContact = () => {
     return contacts.filter(contact => {
       return contact.name.toLowerCase().includes(filter.trim().toLowerCase());
     });
   };
 
-  deleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
+  const deleteContact = id => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== id)
+    );
   };
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-    if (parsedContacts) {
-      this.setState({
-        contacts: parsedContacts,
-      });
-    }
-  }
+  useEffect(
+    prevContacts => {
+      if (contacts !== prevContacts) {
+        localStorage.setItem('contacts', JSON.stringify(contacts));
+      }
+    },
+    [contacts]
+  );
 
-  componentDidUpdate(prevProps, prevState) {
-    console.log('didupdate');
+  return (
+    <div className={s.div}>
+      <h1>Phonebook</h1>
+      <ContactForm addContact={formSubmit} />
 
-    if (this.state.contacts !== prevState.contacts) {
-      console.log('updated');
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  render() {
-    return (
-      <div className={s.div}>
-        <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.formSubmit} />
-
-        <h2>Contacts</h2>
-        <Filter
-          filter={this.filter}
-          filterInputChange={this.handleInputChange}
-        />
-        <ContactList
-          contacts={this.findContact()}
-          deleteContact={this.deleteContact}
-        />
-      </div>
-    );
-  }
-}
+      <h2>Contacts</h2>
+      <Filter filter={filter} filterInputChange={handleInputChange} />
+      <ContactList contacts={findContact()} deleteContact={deleteContact} />
+    </div>
+  );
+};
